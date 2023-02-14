1ea7a2e7f6d3118022376522e910f943d4d8d4ce
praveen <praveenarya518@gmail.com>
2023-02-14 16:43:37 +0530

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Praveen`s Portfolio</title>
+        <title>Praveen YP</title>
       </Head>
 
       {/* Header */}
