4cf1d0263ba63233533a7ac35f9d42507c790e09
praveen <praveenarya518@gmail.com>
2023-02-14 14:15:24 +0530

    push

--- components/ExperienceCard1.tsx
+++ components/ExperienceCard1.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { motion } from "framer-motion";
+import exp1 from "../assets/Nextep logo.jpg"
+import Image from "next/image";
 type Props = {};
 
 function ExperienceCard({}: Props) {
   return (
     <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 transition-opacity duration-200 overflow-hidden">
-      <motion.img
-        initial={{
-          y: -100,
-          opacity: 0,
-        }}
-        transition={{ duration: 1.2 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
+      <Image
+        
         className="sm:w-32 w-20 sm:h-32 h-20 rounded-full  xl:w-[200px] xl:h-[200px] object-cover object-center"
-        src="https://media.licdn.com/dms/image/C510BAQGPfT6chR598w/company-logo_200_200/0/1557677748827?e=1680739200&v=beta&t=hiJfB3WZ-llLYA-9R5ICAuTNdWGNRRsP1WO0Ks6L-Pw"
+        src={exp1}
         alt="exp"
       />
       <div className="px-0 md:px-10">
@@ -22,30 +17,12 @@ function ExperienceCard({}: Props) {
         <p className="font-bold sm:text-2xl text-sm  mt-1">
           NEXTEP ENGINEERING PVT LTD
         </p>
-        <div className="flex space-x-2 my-2">
-         
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8ZvXAFs02fg19pmpZiXpnFAi_1zaUqVQRjrcGNWMV40TvD5n_jBPRX1CFtw43Y_onExE&usqp=CAU"
-            alt="react"
-          />
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://logos-world.net/wp-content/uploads/2022/07/Java-Logo.png"
-            alt="react"
-          />
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png"
-            alt="react"
-          />
         
-        </div>
         <p className="uppercase py-4 sm:text-sm text-xs text-gray-300">JAN 01 2023 - PRESENT</p>
         <ul className="list disc space-y-4 ml-5 sm:text-lg text-xs">
           <li>Utilized  software engineering expertise to develop product throughout the software lifecycle to boost business efficiency, from ideation and requirements definition through development and successful deployment. </li>
-          <li>Enhanced the application's features to effectively fix the bug and optimize the overall performance, reliability and efficiency. </li>
-          <li>Demonstrated expertise in transiating the client's specific needs into easy-to-understand software solution during sales presentation. </li>
+          <li>Enhanced the applications features to effectively fix the bug and optimize the overall performance, reliability and efficiency. </li>
+          <li>Demonstrated expertise in transiting the clients specific needs into easy-to-understand software solution during sales presentation. </li>
          
         </ul>
       </div>
--- components/ExperienceCard2.tsx
+++ components/ExperienceCard2.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { motion } from "framer-motion";
+import exp2 from "../assets/edyoda_transparent_logo.55bcc00f.png"
+import Image from "next/image";
 type Props = {};
 
 function ExperienceCard({}: Props) {
   return (
     <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 transition-opacity duration-200 overflow-hidden">
-      <motion.img
-        initial={{
-          y: -100,
-          opacity: 0,
-        }}
-        transition={{ duration: 1.2 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
+      <Image
+        
+         
         className="sm:w-32 w-20 sm:h-32 h-20 rounded-full  xl:w-[200px] xl:h-[200px] object-cover object-center"
-        src="https://p.urbanpro.com/tv-prod/member/photo/6691673-medium190ap.jpg"
+        src={exp2}
         alt="exp"
       />
       <div className="px-0 md:px-10">
@@ -22,40 +18,7 @@ function ExperienceCard({}: Props) {
         <p className="font-bold sm:text-2xl text-sm mt-1">
           EDYODA (Zekelabs Technologies Pvt Ltd.)
         </p>
-        <div className="flex space-x-2 my-2">
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://reactjs.org/logo-og.png"
-            alt="react"
-          />
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-            alt="react"
-          />
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8ZvXAFs02fg19pmpZiXpnFAi_1zaUqVQRjrcGNWMV40TvD5n_jBPRX1CFtw43Y_onExE&usqp=CAU"
-            alt="react"
-          />
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png"
-            alt="react"
-          />
-
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png"
-            alt="react"
-          />
-
-          <img
-            className="sm:h-10 h-5 sm:w-12 w-7 rounded-full"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425"
-            alt="react"
-          />
-        </div>
+       
         <p className="uppercase py-4 text-gray-300 sm:text-sm text-xs">
           JUN 15 2022 - DEC 30 2022
         </p>
